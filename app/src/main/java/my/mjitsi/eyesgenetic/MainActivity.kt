40bccfb4d6ab6e8422d5469cd1a967db31bbd209
koassumo <0770407@gmail.com>
2022-09-17 02:50:52 +0300

    Initial commit

---
+++ app/src/main/java/my/mjitsi/eyesgenetic/MainActivity.kt
@@ -0,0 +1,18 @@
+package my.mjitsi.eyesgenetic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.mjitsi.eyesgenetic.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
